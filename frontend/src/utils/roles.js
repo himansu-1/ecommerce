d8420a47445064f470f3b2a37f9857de67bb49db
himansu-hsn <himansu.sahu@hsntek.com>
2025-08-23 05:25:41 +0530

    third commit

--- frontend/src/utils/roles.js
+++ frontend/src/utils/roles.js
@@ -4,4 +4,5 @@ export const ROLES = {
   ADMIN: 'admin'
 };
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'; // change to prod URL if needed
+// export const API_BASE_URL = 'http://localhost:5000/api'; // change to prod URL if needed
+export const API_BASE_URL = 'https://ecommerce-642n.onrender.com/api'; // change to prod URL if needed
